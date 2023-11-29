5883a66f450b7728982a824824bc11d0513e8c86
samolego <34912839+samolego@users.noreply.github.com>
2023-11-29 08:46:54 +0100

    Add more SQL insertions

--- api/src/main/resources/sql-scripts/init-db.sql
+++ api/src/main/resources/sql-scripts/init-db.sql
@@ -7,10 +7,25 @@ INSERT INTO kinoteka (ime, spletna_stran) VALUES ('Hollywood Cinemas', 'www.holl
 
 -- public.uporabnik
 INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('alice@gmail.com', 'Alice', 'Smith', 'alice');
-INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('bob@yahoo.com', 'Bob', 'Johnson', 'bob');
 INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('carol@outlook.com', 'Carol', 'Williams', 'carol');
+INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('olivia@aol.com', 'Olivia', 'Hernandez', 'olivia');
+INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('henry@outlook.com', 'Henry', 'Martinez', 'henry');
+INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('isabella@hotmail.com', 'Isabella', 'Hernandez', 'isabella');
+INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('rachel@outlook.com', 'Rachel', 'Wilson', 'rachel');
+INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('jack@aol.com', 'Jack', 'Lopez', 'jack');
+INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('kate@gmail.com', 'Kate', 'Gonzalez', 'kate');
+INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('leonard@yahoo.com', 'Leonard', 'Wilson', 'leonard');
+INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('mary@outlook.com', 'Mary', 'Anderson', 'mary');
+INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('nathan@hotmail.com', 'Nathan', 'Martinez', 'nathan');
+INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('peter@gmail.com', 'Peter', 'Lopez', 'peter');
+INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('quinn@yahoo.com', 'Quinn', 'Gonzalez', 'quinn');
 INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('dave@hotmail.com', 'Dave', 'Brown', 'dave');
 INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('eve@aol.com', 'Eve', 'Davis', 'eve');
+INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('frank@gmail.com', 'Frank', 'Wilson', 'frank');
+INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('grace@yahoo.com', 'Grace', 'Anderson', 'grace');
+INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('sam@hotmail.com', 'Sam', 'Anderson', 'sam');
+INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('bob@yahoo.com', 'Bob', 'Johnson', 'bob');
+INSERT INTO uporabnik (email, ime, priimek, uporabnisko_ime) VALUES ('thomas@aol.com', 'Thomas', 'Martinez', 'thomas');
 
 -- public.zanr
 INSERT INTO zanr (ime) VALUES ('Action');
@@ -18,6 +33,8 @@ INSERT INTO zanr (ime) VALUES ('Comedy');
 INSERT INTO zanr (ime) VALUES ('Drama');
 INSERT INTO zanr (ime) VALUES ('Sci-Fi');
 INSERT INTO zanr (ime) VALUES ('Horror');
+INSERT INTO zanr (ime) VALUES ('Crime');
+INSERT INTO zanr (ime) VALUES ('Animation');
 
 -- public.film
 INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2023-01-01', 'Inception', 'A mind-bending thriller', 4, 4);
@@ -25,6 +42,26 @@ INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2023-02-15',
 INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2023-03-30', 'Avengers: Endgame', 'Epic superhero battle', 4, 1);
 INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2023-04-10', 'Parasite', 'A dark comedy thriller', 5, 2);
 INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2023-05-20', 'The Conjuring', 'Horror movie with jump scares', 2, 5);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2023-06-05', 'The Matrix', 'Sci-fi action', 4, 4);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2023-07-18', 'The Shawshank Redemption', 'Drama about hope and survival', 5, 6);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2023-08-22', 'The Godfather', 'Classic crime drama', 5, 6);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2023-09-10', 'Pulp Fiction', 'Quirky crime tale', 4, 6);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2023-10-14', 'Interstellar', 'Sci-fi odyssey', 4, 4);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2023-11-28', 'Forrest Gump', 'Heartwarming drama', 5, 6);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2023-12-30', 'The Dark Knight', 'Superhero crime thriller', 5, 3);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2024-01-12', 'Fight Club', 'Mind-bending drama', 4, 6);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2024-02-20', 'The Silence of the Lambs', 'Suspenseful psychological thriller', 5, 5);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2024-03-25', 'The Prestige', 'Mystery drama', 4, 6);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2024-04-30', 'Gladiator', 'Epic historical drama', 5, 6);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2024-05-10', 'The Departed', 'Gritty crime thriller', 4, 6);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2024-06-15', 'The Green Mile', 'Emotional drama', 5, 6);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2024-07-20', 'The Usual Suspects', 'Twisty crime mystery', 4, 6);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2024-08-28', 'The Lion King', 'Animated family classic', 5, 7);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2024-09-05', 'Inglourious Basterds', 'Alternate history war film', 4, 6);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2024-10-10', 'The Shining', 'Psychological horror', 5, 5);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2024-11-15', 'The Truman Show', 'Satirical comedy-drama', 4, 6);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2024-12-20', 'Goodfellas', 'Gangster crime film', 5, 6);
+INSERT INTO film (datum_izida, ime, opis, rating, zanr_id) VALUES ('2025-01-25', 'The Avengers', 'Superhero ensemble', 4, 1);
 
 -- public.film_kinoteka
 INSERT INTO film_kinoteka (film_id, kinoteke_id) VALUES (1, 1);
